c01fde0b02a94fd5a04c780759535d08ad12cb7f
Eslam Elgamal <me@esl4m.com>
2019-11-19 15:31:54 +0300

    Adds 1 test case for Borrower api

--- borrower_api/tests.py
+++ borrower_api/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Borrower
 
-# Create your tests here.
+
+class BorrowerTest(TestCase):
+    """ Test module for Borrower model """
+
+    def setUp(self):
+        self.borrower_url = '/api/borrower/'
+        self.client = Client()
+
+        self.name = 'borrower_1'
+
+        Borrower.objects.create(
+            name='borrower_1', amount=5000.0, period=6)
+
+
+    def test_get_all_borrowers(self):
+        response = self.client.get(self.borrower_url)
+        self.assertEqual(response.status_code, 200)
